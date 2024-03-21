b5b7b83de37a5306757e2e4b38ee9af25b705407
Samuel Thibault <samuel.thibault@ens-lyon.org>
2024-03-22 00:50:23 +0100

    Add speakupconf.conf modprobe.d script
    So that root preferences are loaded at startup.

--- Defaults.mk
+++ Defaults.mk
@@ -3,9 +3,11 @@ package = speakup-tools
 
 prefix = /usr/local
 bindir = $(prefix)/bin
+libdir = $(prefix)/lib
 sbindir = $(prefix)/sbin
 datadir = $(prefix)/share
 mandir = $(datadir)/man
+modprobedir = $(libdir)/modprobe.d
 pkgdatadir = $(datadir)/$(package)
 
 CHMOD = chmod
